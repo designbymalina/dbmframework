056b88df4dbe7be3438f397d4890d76c25adb92b
DesignByMalina <biuro@dbm.org.pl>
2024-05-27 17:33:12 +0200

    Framework update v2.3.3

--- application/classes/TemplateEngine.php
+++ application/classes/TemplateEngine.php
@@ -51,7 +51,7 @@ class TemplateEngine extends TemplateFeature
         $cachedFile = self::PATH_CACHE . str_replace(array('/', '.phtml'), array('_', ''), $file . '.php');
         $pathFile = self::PATH_VIEW . str_replace('/', DS, $file);
 
-        if (!getenv('CACHE_ENABLED') || !file_exists($cachedFile) || filemtime($cachedFile) < filemtime($pathFile)) {
+        if ((strtolower(getenv('CACHE_ENABLED')) == 'true') || !file_exists($cachedFile) || filemtime($cachedFile) < filemtime($pathFile)) {
             $code = $this->includeFiles($file);
             $code = $this->compileCode($code);
 
