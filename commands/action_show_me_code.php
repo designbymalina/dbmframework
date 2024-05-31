ad589241608a4273ee326dfff962d17673795039
DesignByMalina <biuro@dbm.org.pl>
2024-05-31 11:14:45 +0200

    Console Commands created v2.3.4

---
+++ commands/action_show_me_code.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+function exampleCode(): string
+{
+    return '<PIPCode>';
+}
+
+echo exampleCode();
+?>
---
+++ commands/action_show_me_text.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+function exampleText(): string
+{
+    return 'PIP Text';
+}
+
+echo exampleText();
+?>
